e2b32b5cfd50ae6a150bd01ffdfdf39cddedbd4e
AllyBrave <monkss@ukr.net>
2020-04-18 18:11:25 +0300

    https

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-fetch('http://api.openweathermap.org/data/2.5/weather?q=Zaporozhe,UA&appid=88d58c6fd9c63e17483c1b3d0957d287')
+fetch('https://api.openweathermap.org/data/2.5/weather?q=Zaporozhe,UA&appid=88d58c6fd9c63e17483c1b3d0957d287')
   .then(function (resp) {return resp.json() })
   .then(function (data) {
     document.querySelector('.city-name').textContent = data.name;
